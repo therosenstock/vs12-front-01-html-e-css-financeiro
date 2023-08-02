6ab852bcb47309cdd1d44c598229fb78d175fcc4
Fabiola <fabiolars2009@hotmail.com>
2023-08-02 17:16:46 -0300

    render transacoes

--- js/account.js
+++ js/account.js
@@ -1,5 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const id = parseInt(params.get("id")); // Convertemos o ID para um número inteiro
+let icones = [];
 
 const url = `https://my-json-server.typicode.com/therosenstock/vs12-front-01-html-e-css-financeiro/users/${id}`;
 
@@ -43,4 +44,34 @@ function render(user) {
   round = round.toFixed(2);
   round = round.replace(".", ",");
   saldo.innerHTML = `R$ ${round}`;
+  renderTransacoes(user.transacoesDebito);
+}
+
+function renderTransacoes(transacoes) {
+  const container = document.getElementById("list-transaction");
+  let list = "";
+  transacoes.forEach((transacao) => {
+    list += `<div class="transaction">
+    <div class="content-aside">
+      <div class="transaction-icon">
+        <img src="../assets/ico-01.svg" alt="" srcset="">
+      </div>
+      <div class="transaction-info">
+        <span class="notice">${transacao.tipo}</span>
+        <h2 class="transaction-title">
+          ${transacao.destino}
+        </h2>
+      </div>
+    </div>
+    <div class="transation-value">
+      <h2 class="value-title ${transacao.classe}">
+        - R$ 50,00
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#001">
+          <i class="ph-fill ph-eye"></i>
+        </button>
+      </h2>
+    </div>
+  </div>`;
+  });
 }
