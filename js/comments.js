fcbf530d95a8d46489613dc86bb7d03d9b40c707
Monara Calado <monaracalado@gmail.com>
2023-08-02 19:12:47 -0300

    ajustando cards

--- js/comments.js
+++ js/comments.js
@@ -1,32 +1,36 @@
 function criarCardComentario(comentario) {
   const cardHTML = `
-      <div class="col-md-4 mb-4">
-          <div class="card">
-              <div class="card-body">
-                  <h5 class="card-title">${comentario.titulo}</h5>
-                  <p class="card-text"><strong>${comentario.nome}</strong> - Avaliação: ${comentario.nota}</p>
-                  <p class="card-text">${comentario.comentario}</p>
-              </div>
+          <div class="card m-5 " style="width: 14rem; height: 19rem">
+            <div class="card-header">
+              Avaliações dos clientes
+            </div>
+          
+            <div class="card-body">
+                <h5 class="card-title">${comentario.titulo}</h5>
+                <p class="card-text"><strong>${comentario.nome}</strong> - Avaliação: ${comentario.nota}</p>
+                <p class="card-text">${comentario.comentario}</p>
+            </div>
           </div>
-      </div>
   `;
   return cardHTML;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const commentsRow = document.getElementById('commentsRow');
-  commentsRow.innerHTML = ''; 
-  
-  fetch('https://my-json-server.typicode.com/therosenstock/vs12-front-01-html-e-css-financeiro/comments')
-      .then(response => response.json())
-      .then(data => {
-          const comentarios = data;
-          comentarios.forEach(comentario => {
-              const card = criarCardComentario(comentario);
-              commentsRow.innerHTML += card;
-          });
-      })
-      .catch(error => {
-          console.error('Erro ao buscar os comentários:', error.message);
+document.addEventListener("DOMContentLoaded", () => {
+  const commentsRow = document.getElementById("comments");
+  commentsRow.innerHTML = "";
+
+  fetch(
+    "https://my-json-server.typicode.com/therosenstock/vs12-front-01-html-e-css-financeiro/comments"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const comentarios = data;
+      comentarios.forEach((comentario) => {
+        const card = criarCardComentario(comentario);
+        commentsRow.innerHTML += card;
       });
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar os comentários:", error.message);
+    });
 });
